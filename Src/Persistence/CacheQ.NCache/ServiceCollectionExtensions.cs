0d863d2116d8abecab9c583097f071b6f27de1e3
Mojtaba Khooryani <m.khooryani@outlook.com>
2022-03-16 21:47:49 +0100

    add NCache

---
+++ Src/Persistence/CacheQ.NCache/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Alachisoft.NCache.Caching.Distributed;
+using Alachisoft.NCache.Caching.Distributed.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CacheQ.NCache;
+
+public static class ServiceCollectionExtensions
+{
+    public static ICacheQConfigurator UseStackExchangeRedisCache(
+        this ICacheQConfigurator cacheQConfigurator,
+        Action<NCacheConfiguration> configure)
+    {
+        cacheQConfigurator.Services.AddNCacheDistributedCache(configure);
+
+        return cacheQConfigurator;
+    }
+}
